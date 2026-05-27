4997e0a61e54685c4e876d9c5c7eb93e2e8101c8
ZexDev4 <158588614+ZexDev4@users.noreply.github.com>
2026-05-27 18:24:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://cbt-tes.vercel.app',
+        changeOrigin: true,
+      }
+    }
+  }
 })
